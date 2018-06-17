607865ff411f382596567604e6d37e8e383539e5
Warryn Evins <W.Hatmaker@yahoo.com>
2018-06-17 12:34:34 -0500

    i think i understand now
    got the buttons to toggle the events on the page up and running

--- first-lab/src/testapp.jsx
+++ first-lab/src/testapp.jsx
@@ -12,17 +12,29 @@ export default class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            text: 'what ever text here'
+            text: 'what ever text here',
+            hasLoaded: false
         }
     }
 
     render() {
         return (
-            <div>
-                <h1>{this.props.headerValue}</h1>
-                <h2>{this.state.text}</h2>
-                <input type="text" placeholder={this.state.text} />  
-            </div>
+            <React.Fragment>
+                {this.state.hasLoaded &&
+                    <div>
+                        <h1>{this.props.headerValue}</h1>
+                        <h2>{this.state.text}</h2>
+                        <input type="text" placeholder={this.state.text} onChange={() => this.setState({ inputValue: 'test' })} />
+                        <button onClick={() => this.setState({ hasLoaded: !this.state.hasLoaded })}>click</button>
+                    </div>
+                }
+                {!this.state.hasLoaded &&
+                    <div>
+                        <h1>Loading...</h1>
+                        <button onClick={() => this.setState({ hasLoaded: !this.state.hasLoaded })}>click</button>
+                    </div>
+                }
+            </React.Fragment>
         )
     }
 }
