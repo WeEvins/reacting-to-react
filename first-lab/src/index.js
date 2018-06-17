d6c1a90340a5940dbf8397ed806b889144bf9f98
Warryn Evins <W.Hatmaker@yahoo.com>
2018-06-17 11:30:55 -0500

    finally started getting this done
    working to understand it

--- first-lab/src/index.js
+++ first-lab/src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './testapp'
+import App from './testapp';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App headerValue='tester header stuff' /> document.getElementById('root'));
 registerServiceWorker();
+
+// Create App component, return simple html
+// Render component from index.js
+// pass prop from index.js to App, place value in html
+// comment out functional component, recode as class component
+// initialize state with string called 'text', assign arbitrary value
+
+// use data from props and state in html
+// add input to app, use component state to define placeholder
+// on input change, set new property on App state
+// replace wrappers with <React.Fragment>
+// add button beneath input
+// add hasLoaded state, init false
+// btn click toggles hasLoaded state
+// only return html if hasLoaded === true
+// display "Loading..." and identical btn if hasLoaded === false
+// on componentDidMount, set hasLoaded === trues
+
--- first-lab/src/testapp.jsx
+++ first-lab/src/testapp.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
-
 import './index.css';
 
+// const App = (props) => {
+//     return (
+//         <h1>{props.headerValue}</h1>
+//     )
+// }
+// export default App;
 
-const App = (props) => {
-    return <h1>I am testing React. {props.name}</h1>;
+export default class App extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
+    render() {
+        return (
+            <h1>{this.props.headerValue}</h1>
+        )
+    }
 }
-
-
-export default App;
